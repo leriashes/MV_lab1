fd1f3136c4a1adb85d7d0e61d2d7e0ba229aff40
leriashes <valeriya-shevchenko-2002@mail.ru>
2024-09-05 13:29:17 +0700

    Чтение матрицы из файла

--- MV_lab1.cpp
+++ MV_lab1.cpp
@@ -1,10 +1,103 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
 using namespace std;
+
+//чтение файла
+bool readFile(vector <vector <double> >& matrix)
+{
+	int str_start = 0, str_end = 1, str_num = -1;
+	double number;
+	bool result = false, end = false;
+	string str;
+
+	//открытие файла с исходными данными
+	ifstream f("input.txt");
+
+	//файл для чтения не открылся
+	if (!f)
+	{
+		cout << " Не удалось открыть файл 'input.txt'" << endl;
+		result = true;
+	}
+
+	if (!result)
+	{
+		//заполнение матрицы системы уравнений
+		while (str_start != str_end && !end)
+		{
+			str_start = f.tellg();
+			getline(f, str);
+			str_end = f.tellg();
+			str_end -= 2;
+			f.seekg(str_start);
+
+			if (str_start != str_end)
+			{
+				matrix.push_back(vector <double>());
+				str_num++;
+			}
+
+			while (f.tellg() < str_end)
+			{
+				f >> number;
+				matrix[str_num].push_back(number);
+			}
+
+			getline(f, str);
+
+			if (str_num > matrix[0].size()) {
+				end = true;
+			}
+		}
+
+		if (!end)
+		{
+			str_num = 0;
+
+			//заполнение столбца свободных членов
+			while (!f.eof())
+			{
+				f >> number;
+				matrix[str_num++].push_back(number);
+			}
+
+			//проверка числа строк и столбцов
+			for (int i = 1; i < str_num && !end; i++)
+			{
+				if (matrix[0].size() != matrix[i].size())
+					end = true;
+			}
+
+			if (str_num + 1 != matrix[0].size())
+				end = true;
+		}
+
+		f.close();
+
+		if (end)
+		{
+			cout << "Неверный формат данных!" << endl;
+			result = true;
+		}
+	}
+
+	return result;
+}
+
 
 int main()
 {
 	setlocale(LC_ALL, "Rus");
+
+	vector <vector <double> > matrix;
+
+	if (readFile(matrix))
+	{
+		return -1;
+	}
 
 	return 0;
 }
